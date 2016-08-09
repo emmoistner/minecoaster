23bf87c384623ef5d272cefa9a4f927cd5f26d8e
Ethan Moistner <emmoistner@gmail.com>
2016-08-09 00:10:19 -0400

    Initial Commit for minecoaster

---
+++ src/components/stuff.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import { Card, CardTitle, CardText } from 'material-ui/Card'
+import { List, ListItem } from 'material-ui/List'
+class Stuff extends Component {
+
+  renderList() {
+    return this.props.players.map(player => {
+      return <ListItem key={player.name} primaryText={player.name} />
+    })
+  }
+
+  render () {
+    return (
+      <div style={{margin: '0 25%'}}>
+        <Card style={{marginTop: '25px'}}>
+          <CardTitle title='Players' subtitle={`${this.props.players.length}/${this.props.maxplayers}`} />
+          <CardText>
+            { this.props.players.length > 0 &&
+              <List>
+                {this.renderList()}
+              </List>
+            }
+          </CardText>
+        </Card>
+        <Card style={{marginTop: '25px'}}>
+          <CardTitle title='Version' subtitle={this.props.version} />
+        </Card>
+        <Card style={{marginTop: '25px'}}>
+          <CardTitle title='Description' subtitle={this.props.description} />
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default Stuff
---
+++ src/main.js
@@ -0,0 +1,34 @@
+/**
+ * App entry point
+ */
+
+// Polyfill
+import 'babel-polyfill';
+
+// Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Routes
+import Routes from './common/components/Routes';
+
+// Base styling
+import './common/base.css';
+
+
+// ID of the DOM element to mount app on
+const DOM_APP_EL_ID = 'app';
+
+// Render the router
+ReactDOM.render((
+  <Router history={browserHistory}>
+    {Routes}
+  </Router>
+), document.getElementById(DOM_APP_EL_ID));
